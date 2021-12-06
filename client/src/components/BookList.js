f9044ab5b7adc6a276c9f982bc530de2c0b417cd
Andrea Bonatti <andreabonatti92@gmail.com>
2021-12-06 12:10:23 +0100

    Making queries from React

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
+import {gql} from 'apollo-boost';
+import {graphql} from 'react-apollo';
+//util to use console.log
+const util = require('util');
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`
 
 class BookList extends Component {
   render() {
+    console.log(this.props);
     return(
       <div>
         <ul id="book-list">
@@ -12,4 +26,4 @@ class BookList extends Component {
   }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+//allow cross-origin requests
+app.use(cors());
 
 //connection to mlab database
 mongoose.connect('mongodb+srv://admin:Test1234@gql-ninja.lcx93.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
