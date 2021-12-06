78455cf742ec7e8b34a8b0b5482fd9aec492c515
Andrea Bonatti <andreabonatti92@gmail.com>
2021-12-06 09:42:40 +0100

    Connected to mlab trought mongoose module

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//connection to mlab database
+mongoose.connect('mongodb+srv://admin:Test1234@gql-ninja.lcx93.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connection.once('open',()=>{
+  console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
