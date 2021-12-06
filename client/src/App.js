4ab949991fa0c24e310f3209051804a8386726ec
Andrea Bonatti <andreabonatti92@gmail.com>
2021-12-06 11:28:07 +0100

    Creation of the BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-
-
+//components
+import BookList from './components/BookList'
 
 function App() {
   return (
     <div className="main">
-    <h1>Ninja's Reading List</h1>
+      <h1>Ninja's Reading List</h1>
+      <BookList/>
     </div>
   );
 }
